b13ecb42b1d6e59d167767d9974b4cb90b899fff
ashkan <cannon@skule.ca>
2013-07-18 18:52:55 -0400

    Changing folder structure.

--- adapt/searchform.php
+++
@@ -1,7 +0,0 @@
-<!--BEGIN .searchform-->
-<form method="get" class="searchform" action="<?php echo home_url(); ?>/">
-	<fieldset>
-		<input type="text" name="s" class="s" value="<?php _e('To search type and hit enter', 'engine') ?>" onfocus="if(this.value=='<?php _e('To search type and hit enter', 'engine') ?>')this.value='';" onblur="if(this.value=='')this.value='<?php _e('To search type and hit enter', 'engine') ?>';" />
-	</fieldset>
-<!--END .searchform-->
-</form>
